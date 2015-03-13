b5f5fd906b5bf79ce86c29ab3972b3b79acce1f5
MerrittMelker <merrittmelker@gmail.com>
2015-03-13 16:36:32 -0500

    Project generation

---
+++ SignalrWebinar/SignalrWebinar/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SignalrWebinar.Startup))]
+namespace SignalrWebinar
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
