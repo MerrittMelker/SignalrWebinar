53aaaa9038eeae4229181ca3e82fec63e3f2441f
MerrittMelker <merrittmelker@gmail.com>
2015-03-13 19:48:35 -0500

    Added chat hub

---
+++ SignalrWebinar/SignalrWebinar/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalrWebinar.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void SendMessage(string message)
+        {
+            Clients.All.broadcastMessage(message);
+        }
+    }
+}
